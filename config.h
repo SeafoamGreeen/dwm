563525f45dc2b0cb21c4cae8c8e9e0e9091bc87d
SeafoamGreeen <Darkakish@gmail.com>
2022-11-27 10:04:17 -0300

    noborderpatch

--- config.h
+++ config.h
@@ -2,16 +2,16 @@
 
 /* appearance */
 static const unsigned int borderpx       = 3;   /* border pixel of windows */
-static const unsigned int snap           = 32;  /* snap pixel */
+static const unsigned int snap           = 0;  /* snap pixel */
 static const int swallowfloating         = 0;   /* 1 means swallow floating windows by default */
 static const unsigned int gappih         = 0;  /* horiz inner gap between windows */
 static const unsigned int gappiv         = 0;  /* vert inner gap between windows */
 static const unsigned int gappoh         = 0;  /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov         = 0;  /* vert outer gap between windows and screen edge */
-static const int smartgaps_fact          = 1;   /* gap factor when there is only one client; 0 = no gaps, 3 = 3x outer gaps */
+static const int smartgaps_fact          = 0;   /* gap factor when there is only one client; 0 = no gaps, 3 = 3x outer gaps */
 static const int showbar                 = 1;   /* 0 means no bar */
 static const int topbar                  = 0;   /* 0 means bottom bar */
-static const int bar_height              = 30;   /* 0 means derive from font, >= 1 explicit height */
+static const int bar_height              = 0;   /* 0 means derive from font, >= 1 explicit height */
 static int floatposgrid_x                = 5;  /* float grid columns */
 static int floatposgrid_y                = 5;  /* float grid rows */
 /* Status is to be shown on: -1 (all monitors), 0 (a specific monitor by index), 'A' (active monitor) */
@@ -28,7 +28,9 @@ static int tiledindicatortype            = INDICATOR_NONE;
 static int floatindicatortype            = INDICATOR_TOP_LEFT_SQUARE;
 static int fakefsindicatortype           = INDICATOR_PLUS;
 static int floatfakefsindicatortype      = INDICATOR_PLUS_AND_LARGER_SQUARE;
-static const char *fonts[]               = { "Fira Code Nerd Font:size=9:style=Bold","Fira Code Nerd Font:size=16" };
+/* static const char *fonts[]               = { "Fira Code Nerd Font:size=9:style=Bold","Fira Code Nerd Font:size=16" }; */
+static const char *fonts[]               = { " Terminus:size=13","Fira Code Nerd Font:size=16" };
+/* static const char *fonts[]               = { " monospace:size=10","Fira Code Nerd Font:size=16" }; */
 static const char dmenufont[]            = "monospace:size=10";
 
 static char c000000[]                    = "#000000"; // placeholder value
@@ -267,6 +269,8 @@ static const char *mousecmd[]  = {"mouseless","-c","/home/seafoam/.config/mousel
 
 static const char *roficmd[] = { "rofi", "-show", "combi", NULL };
 
+static const char *wallcmd[] = { "wallchange", NULL };
+
 static Key keys[] = {
 	/* modifier                     key            function                argument */
 	{ MODKEY,                       XK_Escape,     setkeymode,             {.ui = COMMANDMODE} },
@@ -288,8 +292,8 @@ static Key keys[] = {
 	{ MODKEY|ControlMask,                       XK_j,          setcfact,               {.f = -0.05} },
 	{ MODKEY,                       XK_o,          setcfact,               {0} },
 
-	{ MODKEY|ControlMask|ShiftMask, XK_e,          aspectresize,           {.i = +24} },
-	{ MODKEY|ControlMask|ShiftMask, XK_r,          aspectresize,           {.i = -24} },
+	{ MODKEY|Mod1Mask,              XK_plus,       aspectresize,           {.i = +24} },
+	{ MODKEY|Mod1Mask,              XK_minus,      aspectresize,           {.i = -24} },
 
 	/* { MODKEY|Mod4Mask,              XK_Down,       moveresize,             {.v = "0x 25y 0w 0h" } }, */
 	/* { MODKEY|Mod4Mask,              XK_Up,         moveresize,             {.v = "0x -25y 0w 0h" } }, */
@@ -306,8 +310,8 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_Return,     zoom,                   {0} },
 
 /* gaps */
-	{ MODKEY|Mod1Mask,              XK_plus,       incrgaps,               {.i = +3 } },
-	{ MODKEY|Mod1Mask,              XK_minus,      incrgaps,               {.i = -3 } },
+	/* { MODKEY|Mod1Mask,              XK_plus,       incrgaps,               {.i = +3 } }, */
+	/* { MODKEY|Mod1Mask,              XK_minus,      incrgaps,               {.i = -3 } }, */
 	/* { MODKEY|Mod1Mask,              XK_u,          incrgaps,               {.i = +1 } }, */
 	/* { MODKEY|Mod1Mask|ShiftMask,    XK_u,          incrgaps,               {.i = -1 } }, */
 	/* { MODKEY|Mod1Mask,              XK_i,          incrigaps,              {.i = +1 } }, */
@@ -391,9 +395,16 @@ static Key keys[] = {
 	{ MODKEY,                       XK_0,          view,                   {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,          tag,                    {.ui = ~0 } },
 
-	{ MODKEY,             XK_F5,         xrdb,                   {.v = NULL } },
+	{ Mod3Mask,             XK_Pause,         xrdb,                   {.v = NULL } },
 
   {MODKEY|ShiftMask, XK_s, spawn, SHCMD("flameshot gui") },
+  {MODKEY|ShiftMask, XK_p, spawn, SHCMD("picom || killall picom") },
+
+  {MODKEY, XK_Left, spawn, SHCMD("xrandr --output eDP-1 --rotate left") },
+  {MODKEY, XK_Right, spawn, SHCMD("xrandr --output eDP-1 --rotate right") },
+  {MODKEY, XK_Up, spawn, SHCMD("xrandr --output eDP-1 --rotate inverted") },
+  {MODKEY, XK_Down, spawn, SHCMD("xrandr --output eDP-1 --rotate normal") },
+  {Mod1Mask|ShiftMask, XK_w, spawn,                  {.v = wallcmd } },
   {Mod1Mask, XK_Tab, spawn,                  {.v = mousecmd } },
 
 	/* { MODKEY,                       XK_comma,      focusmon,               {.i = -1 } }, */
